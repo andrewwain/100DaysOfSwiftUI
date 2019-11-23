754c071accad23e5b2978dc6c04f95deb4b33290
Andrew Wain <andrewwain@gmail.com>
2019-11-23 22:04:59 +0000

    Fetch the data and parse it into User and Friend structs.

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State private var users = [User(isActive: false, name: "Andrew", age: 33, company: "None", email: "andrew@example.com", address: "Somewhere", about: "Smells", registered: Date(), tags: ["none"], friends: [Friend]())]
+    @State private var users = [User]()
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(self.users) { user in
+                HStack {
+                    Text("\(user.name)")
+                }
+            }.navigationBarTitle("Friend Face")
+        } .onAppear(perform: GetUsers)
+    }
+    
+    func GetUsers() {
+        let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown Error").")
+                return
+            }
+
+            if let decodedUsers = try? JSONDecoder().decode([User].self, from: data) {
+                // back to the main thread
+                DispatchQueue.main.async {
+                    // update UI
+                    self.users = decodedUsers
+                }
+            } else {
+                print("Invalid response from server")
+            }
+        }.resume()
     }
 }
 
---
+++ FriendFace/Friend.swift
@@ -0,0 +1,14 @@
+//
+//  Friend.swift
+//  FriendFace
+//
+//  Created by Andrew on 23/11/2019.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import Foundation
+
+struct Friend: Codable {
+    var id: String
+    var name: String
+}
