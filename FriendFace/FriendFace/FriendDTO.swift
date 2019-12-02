fe66bbf69f2c2c4bbda4f7ef1919ae5a6343dac5
Andrew Wain <andrewwain@gmail.com>
2019-12-02 13:11:42 +0000

    files moved

---
+++ FriendFace/FriendFace/FriendDTO.swift
@@ -0,0 +1,14 @@
+//
+//  Friend.swift
+//  FriendFace
+//
+//  Created by Andrew on 23/11/2019.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import Foundation
+
+struct FriendDTO: Codable, Identifiable {
+    var id: String
+    var name: String
+}
---
+++ FriendFace/FriendFace/Tag+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Tag+CoreDataClass.swift
+//  FriendFace
+//
+//  Created by Andrew on 25/11/2019.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Tag)
+public class Tag: NSManagedObject {
+
+}
